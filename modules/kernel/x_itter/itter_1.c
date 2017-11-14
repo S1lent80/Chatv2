15ad91fc753a5c4d79ea4a2555283dda32d92390
Gleb Bair <silentcore32@gmail.com>
2017-11-13 19:00:25 -0500

    Add files via upload

--- modules/kernel/x_itter/itter_1.c
+++ modules/kernel/x_itter/itter_1.c
@@ -1,6 +1,8 @@
 #include "linux/module.h"
 #include "linux/init.h"
 #include "linux/kernel.h"
+// #include "param.h"
+
 
 // ******************************************************************************************
 
@@ -16,7 +18,20 @@
 #define _ce   "\033[0m"
 
 // ******************************************************************************************
+/* Module parameters -> Definitions */
+static char *cmd_str = "<INSERT_COMMAND_HERE>";
+static int itter_id = 0;   // Default
 
+/* Module parameters */
+module_param(cmd_str, charp, 0000);
+module_param(itter_id, int, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
+
+/* Module parameter descriptions */
+MODULE_PARM_DESC(itter_id, "The itteration ID to use to exec sys_itter_function protocol...");
+MODULE_PARM_DESC(cmd_str, "Command line string to be executed...");   // Because why not
+
+
+// ******************************************************************************************
 static int __init mod_start(void){
     printk(KERN_INFO "\n%s[ INFO ]%s%s Initializing program modules...%s\n 1.",_gs,_ce,_ys,_ce);
     // Initialize the modules
