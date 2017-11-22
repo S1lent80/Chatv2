c53a777a59975bfd3fb8cae04a9dd9e03375330b
Gleb Bair <silentcore32@gmail.com>
2017-11-22 13:16:42 -0500

    Add files via upload

--- tools/web-testing/web_dir/sdirb.py
+++ tools/web-testing/web_dir/sdirb.py
@@ -37,7 +37,7 @@ class SDirbMain():
             print gs + "Wordlists: i.e -> /path/to/wordlist          -  Current dir: " + ce + ys + "None" + ce
             print gs + "Port: i.e -> 80                              -  Current port: " + ce + ys + str(port) + ce + "\n"
             sys.exit(1)
-    
+
 
     def _checkHostConnection(self, rhost):
         print astk + gs + "Checking connection to RHOST: " + ce + ys + rhost + ce + gs + "..." + ce
@@ -59,3 +59,45 @@ class SDirbMain():
             sys.exit(1)
 
     def _readWordList(self, wordlist):
+        print astk + gs + "Parsing wordlist: " + ce + ys + wordlist + ce + gs + "..." + ce
+        # Open the file for reading
+        try:
+            with open(wordlist) as file:
+                chk_file = file.read().strip().split('\n')
+                global chk_file
+            print plus + ys + "Successfully parsed wordlist..." + ce
+        except IOError as e:
+            print minus + gs + "Could not parse file: " + wordlist + " - Error: " + str(e) + ce
+            sys.exit(1)
+
+    def webPathScrape(self, path):
+        print astk + gs + "Init web response/request..." + ce + "\n"
+        try:
+            response = requests.get(rhost + "/" + path).status_code
+        except Exception as e:
+            print minus + gs + "Error -> " + ce + ys + str(e) + ce
+            sys.exit(1)
+        if response == 200:
+            print gs + "[ VALID DIRECTORY ]: " + ce + ys + "%s - %s..." % (path, rhost)
+            pass
+
+    def _dirbMain(self):
+        print "\n" + astk + gs + "Beginning web dir scan..." + ce
+        try:
+            for i in range(len(chk_file)):
+                self.webPathScrape(chk_file[i])
+            print "\n" + plus + ys + "Scan complete..." + ce + "\n"
+        except KeyboardInterrupt:
+            print "\n" + gs + "User interrupt..." + ce + "\n"
+            sys.exit(1)
+
+def main():
+    sdirb = SDirbMain()
+
+    sdirb._checkParam(rhost, wordlist, port)
+    sdirb._checkHostConnection(rhost)
+    sdirb._readWordList(wordlist)
+    sdirb._dirbMain()
+
+if __name__ == '__main__':
+    main()
