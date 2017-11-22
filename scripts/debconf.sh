ae00dc46233dcb9375c39fff403df40036f9616a
Gleb Bair <silentcore32@gmail.com>
2017-11-22 09:37:01 -0500

    Add files via upload

--- scripts/debconf.sh
+++ scripts/debconf.sh
@@ -14,9 +14,9 @@ fi
 if [ -d "${FULL_DIR}" ]; then
 	# Scan the package directories
 	echo -e "\n\033[34m[*]\033[0m\033[32m Scanning packages in \"${ARCH}/${OS_TYPE}\"...\033[0m\n"
-	dpkg-scanpackages ${FULL_DIR} | gzip -9c > "${FULL_DIR}/Packages.gz"
+	dpkg-scanpackages ${FULL_DIR} | gzip -9c > ${FULL_DIR}/Packages.gz
 	if [[ $? != 1 ]]; then
-		echo -e "\n\033[32mSuccess...\033[0m"
+		echo -e "\n\033[32mSuccess...\033[0m\n"
 	else
 		echo -e "\n\033[31m[-]\033[0m\033[32m Could not scan packages in \"${ARCH}/${OS_TYPE}\"...\033[0m\n"
 		exit 1
@@ -24,7 +24,30 @@ if [ -d "${FULL_DIR}" ]; then
 
 	# Write to s1.list
 	echo -e "\033[34m[*]\033[0m\033[32m Writing to /etc/apt/sources.list.d/s1.list...\033[0m"
-	echo "deb http://127.0.0.1/debs amd64/debian/" > /etc/apt/sources.list.d/s1.list
+	if [[ -f "/etc/apt/sources.list.d/s1.list" ]]; then
+		echo -e "\n\033[32mApt file: s1.list exists...\033[0m"
+		if [[ `grep "deb http://127.0.0.1/debs amd64/debian/" /etc/apt/sources.list` ]]; then
+			echo -e "\033[32mRepository URL exists in /etc/apt/sources.list...\033[0m\n"
+		else
+			# Prompt the user
+			echo -e "\n"
+			read -p "Add the s1 repo to /etc/apt/sources.list? [Y|y|N|n]: " choice
+			if [[ ${choice} == "Y" ]] || [[ ${choice} == "y" ]]; then
+				echo "deb http://127.0.0.1/debs amd64/debian/" >> /etc/apt/sources.list
+			elif [[ ${choice} == "N" ]] || [[ ${choice} == "n" ]]; then
+				echo -e "\n\033[32mNot adding s1 repo to /etc/apt/sources.list...\033[0m\n"
+			else
+				echo ""
+			fi
+		fi
+	else
+		# echo "deb http://127.0.0.1/debs amd64/debian/" > /etc/apt/sources.list.d/s1.list
+		if [[ `grep "deb http://127.0.0.1/debs amd64/debian/" /etc/apt/sources.list` ]]; then
+			echo -e "\033[32mRepository URL exists in /etc/apt/sources.list...\033[0m\n"
+		else
+			echo "deb http://127.0.0.1/debs amd64/debian/" >> /etc/apt/sources.list
+		fi
+	fi
 
 	# Update the system
 	apt-get update 
